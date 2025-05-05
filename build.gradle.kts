c2d58d77a9a66d163318edfc53d1b85d973dde7b
DeadMaster121 <34813557+DeadMaster121@users.noreply.github.com>
2025-05-05 20:12:01 +0100

    Add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "net.ultranetwork"
@@ -21,6 +22,22 @@ dependencies {
     implementation("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set("Minecraft Visual Renderer")
+                description.set("Generate minecraft like images using builders")
+            }
+        }
+    }
+}
 tasks.test {
     useJUnitPlatform()
 }
